54ddbc5b577b413ad93b957ad1c3d6b06fae019f
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-06-14 20:39:55 +0300

    Initial commit

---
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -0,0 +1,27 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class GoogleStepDefinitions {
+
+    @When("user is on Google search page")
+    public void user_is_on_google_search_page() {
+        // Write code here that turns the phrase above into concrete actions
+        Driver.getDriver().get("https://www.google.com");
+    }
+
+    @Then("user should title is Google")
+    public void user_should_title_is_google() {
+        // Write code here that turns the phrase above into concrete actions
+        String expectedTitle = "Google";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+        Driver.closeDriver();
+    }
+
+}
