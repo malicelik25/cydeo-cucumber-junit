493ebccec6a8be0044db489e104d86100bca4b73
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-06-15 21:05:00 +0300

    day15 Background & Hooks

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -11,9 +11,19 @@ In this class we will be able to pass pre- & post- condition to each scenario an
 public class Hooks {
 
     //import from io.cucumber.java not from junit
-    @Before
+    @Before(order = 1)
     public void setupScenario() {
         System.out.println("===Setting up browser using cucumber @Before");
+    }
+
+    @Before(value = "@login", order = 2)
+    public void setupScenarioForLogins() {
+        System.out.println("===this will only apply to scenarios with @login tag");
+    }
+
+    @Before(value = "@db", order = 0)
+    public void setupForDatabaseScenarios() {
+        System.out.println("===this will only apply to scenarios with @db tag");
     }
 
     @After
