7323a5954c619dbd7c3343e8cd3a6fa1b8b8e44c
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-06-15 12:19:33 +0300

    Login_StepDefinitions

--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -1,6 +1,7 @@
 package com.cydeo.step_definitions;
 
 import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -10,12 +11,25 @@ public class Login_StepDefinitions {
     public void user_enters_librarian_username() {
         System.out.println("User enters librarian username");
     }
+
     @When("user enters librarian password")
     public void user_enters_librarian_password() {
         System.out.println("User enters librarian password");
     }
+
     @Then("user should see the dashboard")
     public void user_should_see_the_dashboard() {
         System.out.println("User should see the dashboard");
+    }
+
+
+    @When("user enters student username")
+    public void userEntersStudentUsername() {
+
+    }
+
+    @And("user enters student password,")
+    public void userEntersStudentPassword() {
+
     }
 }
