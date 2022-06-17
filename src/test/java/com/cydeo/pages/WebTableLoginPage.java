839a5ab912f690d40f59f12a2a5bb99238073fd5
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-06-17 16:23:04 +0300

    day17_Parameterization-Data Tables Review

--- src/test/java/com/cydeo/pages/WebTableLoginPage.java
+++ src/test/java/com/cydeo/pages/WebTableLoginPage.java
@@ -1,5 +1,6 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,7 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class WebTableLoginPage {
 
-    public WebTableLoginPage(){
+    public WebTableLoginPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
@@ -23,11 +24,11 @@ public class WebTableLoginPage {
     /**
      * No parameters.
      * When we call this method, it will directly login using
-     *
+     * <p>
      * Username: Test
      * Password: Tester
      */
-    public void login(){
+    public void login() {
         this.inputUsername.sendKeys("Test");
         this.inputPassword.sendKeys("Tester");
         this.loginButton.click();
@@ -35,15 +36,24 @@ public class WebTableLoginPage {
 
     /**
      * This method will accept two arguments and login.
+     *
      * @param username
      * @param password
      */
-    public void login(String username, String password){
+    public void login(String username, String password) {
         inputUsername.sendKeys(username);
         inputPassword.sendKeys(password);
         loginButton.click();
     }
 
+    /**
+     * This method will login using credentials from configuration.properties
+     */
+    public void loginWithConfig() {
+        inputUsername.sendKeys(ConfigurationReader.getProperty("web.table.username"));
+        inputPassword.sendKeys(ConfigurationReader.getProperty("web.table.pw"));
+        loginButton.click();
+    }
 
 
 }
